48aab8e23aba87735e4043e154504014c7fcfa32
jorvarea <jorvarea@c2r2s1.42malaga.com>
2024-02-14 21:08:00 +0100

    minor fix

--- src/client/handle_timeout.c
+++ src/client/handle_timeout.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 22:01:04 by jorvarea          #+#    #+#             */
-/*   Updated: 2024/02/14 14:28:57 by jorvarea         ###   ########.fr       */
+/*   Updated: 2024/02/14 21:07:06 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 void	handle_timeout(t_packet *packet, int server_pid, int *signal_interval,
 		t_timer *timer)
 {
+	*signal_interval *= 2;
 	ft_printf("\033[0;31m");
 	ft_printf("Timeout. Current signal interval: %d us. ", *signal_interval);
 	ft_printf("Retransmitting...\n");
 	ft_printf("\033[0m");
-	*signal_interval *= 2;
 	send_packet(packet, server_pid, *signal_interval);
 	timer->time = 0;
 }
